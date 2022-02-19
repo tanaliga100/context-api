fde32c615e672b1fbff57361a24e7e8b47bda524
Jordan <jordantanaliga@gmail.com>
2022-02-19 15:27:02 +0800

    first commit

---
+++ useContext-useReducer/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState, useReducer, useRef } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Counter from "./components/counter";
+
+function App() {
+
+  return (
+    <div>
+    <Counter/>
+    </div>
+  );
+} 
+
+
+export default App;
